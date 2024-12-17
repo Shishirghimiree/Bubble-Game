8ae8529b83352dedb52ead67f7aad94350701838
shishirghimiree <shishirghimirecsit077@indrenicollege.edu.np>
2024-12-17 22:36:41 +0545

    Find The Number Game

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+var timer=60;
+var score=0;
+var hitRN;
+function increaseScore(){
+    score +=10;
+    document.querySelector("#scoreVal").innerHTML=score;
+}
+
+function getNewHit(){
+    hitRN=Math.floor(Math.random()*10);
+    document.querySelector("#hitvalue").innerHTML=hitRN;
+}
+
 function makeBubble(){
     var clutter = "";
 for(var i = 1;i<=207;i++){
@@ -7,7 +20,6 @@ for(var i = 1;i<=207;i++){
 document.querySelector("#pbtn").innerHTML = clutter;
 }
 
-var timer=60;
 function runTimer(){
     var timerCount = setInterval(function(){
         if(timer>0){
@@ -15,8 +27,20 @@ function runTimer(){
             document.querySelector("#timerVal").innerHTML= timer;
         }else{
             clearInterval(timerCount);
+            document.querySelector("#pbtn").innerHTML='<h1 id="gameOver">Game Over</h1>';
         }
     },1000);
 }
+
+document.querySelector("#pbtn").addEventListener("click",function(details){
+   var clickedNumber=Number(details.target.textContent);
+   if(clickedNumber===hitRN){
+    increaseScore();
+    makeBubble();
+    getNewHit();
+   }
+})
+
+getNewHit();
 runTimer();
 makeBubble();
